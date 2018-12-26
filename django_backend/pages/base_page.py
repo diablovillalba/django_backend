e679b8c46ea2aadd8edc4532c56024897e2b41c1
aldovillalbarodriguez <44008834+aldovillalbarodriguez@users.noreply.github.com>
2018-12-26 12:09:10 -0600

    Changes to install

--- django_backend/pages/base_page.py
+++ django_backend/pages/base_page.py
@@ -22,7 +22,7 @@ class Base_page():
     Method to get information from json_file to verify json schema
     '''
     def get_file_schema(self, json_file):
-        dir_path = os.path.realpath("resources/"+json_file+".json")
+        dir_path = os.path.realpath("django_backend/resources/"+json_file+".json")
         file_schema = open(dir_path, "r")
         json_file_schema = json.loads(file_schema.read())
         return json_file_schema
